60b59236be0c332dcda0e6db6df8e54afe7737dc
GabrielOzy7 <gabrieldevs.souza@gmail.com>
2023-09-11 21:59:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script  lang="ts">
+import {defineComponent} from 'vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
+import Grid from './components/Grid.vue'
+export default defineComponent({
+  name :'App',
+  components :{
+    Grid
+  }
+})
+
+
+
+</script>
+
+<template>
+ <Grid/>
+  
+</template>
+
---
+++ src/components/Grid.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container-fluid container-md">
+        <div class="row">
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+    name: 'grid-component'
+})
+</script>
+<style lang="">
+    
+</style>
